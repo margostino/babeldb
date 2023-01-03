7caec3f8e8c10429c9d113e03b2e916ee1e4a3ce
Martin D'Agostino <martin.dagostino@klarna.com>
2023-01-03 23:27:52 +0100

    select token type

--- engine/engine.go
+++ engine/engine.go
@@ -1,11 +1,13 @@
 package engine
 
 import (
+	"fmt"
 	"github.com/margostino/babeldb/collector"
 	"github.com/margostino/babeldb/common"
 	"github.com/margostino/babeldb/storage"
 	"github.com/robfig/cron/v3"
 	"github.com/xwb1989/sqlparser"
+	"github.com/xwb1989/sqlparser/dependency/querypb"
 	"strings"
 )
 
@@ -40,14 +42,12 @@ func (e *Engine) createSource(name string, url string, schedule string) {
 	job.Start()
 }
 
-func (e *Engine) selectTokens(sourceName string, whereExpr *sqlparser.Expr) []*storage.Token {
-	conditions := e.getConditions(whereExpr)
+func (e *Engine) selectTokens(sourceName string, conditions map[string]*querypb.BindVariable) []*storage.Token {
 	return e.storage.SelectTokens(sourceName, conditions)
 }
 
-func (e *Engine) getConditions(whereExpr *sqlparser.Expr) map[string]interface{} {
+func (e *Engine) getConditions(whereConditions *map[string]*querypb.BindVariable) map[string]interface{} {
 	conditions := make(map[string]interface{})
-	
 	return conditions
 }
 
@@ -61,17 +61,41 @@ func (e *Engine) Parse(input string) (*sqlparser.Statement, error) {
 			ReplaceAll(" when ", "&").
 			ReplaceAll(";", "").
 			ReplaceAll("'", "").
+			TrimSpace().
 			Split("&").
 			Values()
 
 		e.createSource(parts[0], parts[1], parts[2])
 	} else {
 		statement, err := sqlparser.Parse(input)
+		var bindVars = make(map[string]*querypb.BindVariable)
+		sqlparser.Normalize(statement, bindVars, "")
 
 		if !common.IsError(err, "when parsing SQL input") {
 			switch stmt := statement.(type) {
 			case *sqlparser.Select:
-				results := e.selectTokens("earth", &stmt.Where.Expr)
+				buf := sqlparser.NewTrackedBuffer(nil)
+				stmt.Where.Expr.Format(buf)
+				whereCondition := buf.String()
+
+				var fields = whereCondition
+				var preField = ""
+				whereConditionFields := make(map[string]*querypb.BindVariable, 0)
+				for id, bindVar := range bindVars {
+					fields = common.NewString(fields).
+						ReplaceAll(fmt.Sprintf(" = :%s", id), "").
+						ReplaceAll(" and ", " ").
+						ReplaceAll(preField, "").
+						Value()
+					field := common.NewString(fields).
+						TrimSpace().
+						Split(" ").
+						Values()[0]
+					preField = field
+					whereConditionFields[field] = bindVar
+				}
+
+				results := e.selectTokens("earth", whereConditionFields)
 				println(results)
 				_ = stmt
 			case *sqlparser.Insert:
--- storage/storage.go
+++ storage/storage.go
@@ -1,6 +1,7 @@
 package storage
 
 import (
+	"github.com/xwb1989/sqlparser/dependency/querypb"
 	"golang.org/x/net/html"
 )
 
@@ -35,9 +36,38 @@ func (s *Storage) AddSource(source *Source) {
 	s.sources[source.Name] = source
 }
 
-func (s *Storage) SelectTokens(name string, conditions map[string]interface{}) []*Token {
+func (s *Storage) SelectTokens(name string, conditions map[string]*querypb.BindVariable) []*Token {
+	var tokenType html.TokenType
 	results := make([]*Token, 0)
-	tokenType := conditions[""].(html.TokenType)
+
+	if conditions["type"] != nil {
+		switch value := string(conditions["type"].Value); value {
+		case "text":
+			tokenType = html.TextToken
+			break
+		case "error":
+			tokenType = html.TextToken
+			break
+		case "start_tag":
+			tokenType = html.StartTagToken
+			break
+		case "end_tag":
+			tokenType = html.EndTagToken
+			break
+		case "self_closing_tag":
+			tokenType = html.SelfClosingTagToken
+			break
+		case "comment":
+			tokenType = html.CommentToken
+			break
+		case "doc_type":
+			tokenType = html.DoctypeToken
+			break
+		default:
+			// TODO
+		}
+	}
+
 	for _, token := range s.sources[name].Tokens {
 		if token.Type == tokenType {
 			results = append(results, token)
